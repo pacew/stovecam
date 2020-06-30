385b8f439da9258941e52979120e525eb39043af
Pace Willisson <pace@alum.mit.edu>
2020-06-29 20:25:08 -0400

    baseline

--- MLX90640_I2C_Driver.h
+++ MLX90640_I2C_Driver.h
@@ -17,6 +17,11 @@
 #ifndef _MLX90640_I2C_Driver_H_
 #define _MLX90640_I2C_Driver_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <stdint.h>
 
 
@@ -25,4 +30,10 @@
     int MLX90640_I2CRead(uint8_t slaveAddr,uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data);
     int MLX90640_I2CWrite(uint8_t slaveAddr,uint16_t writeAddress, uint16_t data);
     void MLX90640_I2CFreqSet(int freq);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif
--- ir.c
+++ ir.c
@@ -2,16 +2,34 @@
 #include <stdint.h>
 
 #include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
 #include "i2c.h"
 
-#define MLX_I2C_ADDR (0x33 << 1)
+#define MLX_I2C_ADDR (0x33)
 
 int
 main (int argc, char **argv)
 {
-	static uint16_t eeMLX90640[832];
+//	static uint16_t eeMLX90640[832];
+	uint16_t buf[1000];
+	int i;
 
-	MLX90640_DumpEE(MLX_I2C_ADDR, eeMLX90640);
+	MLX90640_I2CRead(MLX_I2C_ADDR, 0x2400, 16, buf);
+
+	for (i = 0; i < 16; i++) {
+		printf ("%04x ", buf[i]);
+	}
+	printf ("\n");
+
+	MLX90640_I2CRead(MLX_I2C_ADDR, 0x2404, 16, buf);
+
+	for (i = 0; i < 16; i++) {
+		printf ("%04x ", buf[i]);
+	}
+	printf ("\n");
+
+
+//	MLX90640_DumpEE(MLX_I2C_ADDR, eeMLX90640);
 
 	return (0);
 }
